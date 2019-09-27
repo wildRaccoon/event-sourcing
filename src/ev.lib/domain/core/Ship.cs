07c9773de0ec10e44c61d7ef635c3b12258e1000
wildRaccoon <chukvm@gmail.com>
2019-09-27 10:59:14 +0300

    add register event

---
+++ src/ev.lib/domain/core/Ship.cs
@@ -0,0 +1,9 @@
+namespace ev.lib.domain.core
+{
+    public class Ship
+    {
+        public string Name {get; private set;}
+        public string RegistrationNumber {get; private set;}
+        public Port Location {get; private set;}
+    }
+}
