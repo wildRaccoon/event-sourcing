d143a08e11a04167576348e43917f1b0abb00a31
wildRaccoon <chukvm@gmail.com>
2019-10-31 16:00:10 +0200

    add core interfaces

---
+++ src/ev.lib/domain/interfaces/IEventProcessor.cs
@@ -0,0 +1,11 @@
+﻿using ev.lib.domain.events;
+using System.Threading.Tasks;
+
+namespace ev.lib.domain.interfaces
+{
+    public interface IEventProcessor<T> 
+        where T: class
+    {
+        Task<T> ProcessAsync(DomainEvent @event);
+    }
+}
---
+++ src/ev.lib/domain/interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ev.lib.domain.interfaces
+{
+    public interface IRepository
+    {
+        Task AddAsync<T>(T t) where T: class;
+        Task UpdateAsync<T>(T t) where T : class;
+        Task RemoveAsync<T>(T t) where T : class;
+        Task<T> GetAsync<T>(string id) where T : class;
+        Task<List<T>> QueryAsync<T>(Func<T, bool> query) where T : class;
+    }
+}
