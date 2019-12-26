01f6d3f3a775dadd95eb28dbbcf69078bb119504
wildRaccoon <chukvm@gmail.com>
2019-12-26 15:57:20 +0200

    remove not used folder

--- src/ev.lib/domain/interfaces/IRepository.cs
+++ src/ev.lib/domain/interfaces/IRepository.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace ev.lib.domain.interfaces
 {
     public interface IRepository
     {
-        Task AddAsync<T>(T t) where T: class;
+        Task AddAsync<T>(T t) where T : class;
         Task UpdateAsync<T>(T t) where T : class;
         Task RemoveAsync<T>(T t) where T : class;
         Task<T> GetAsync<T>(string id) where T : class;
