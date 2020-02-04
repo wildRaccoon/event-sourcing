2294893102e887c3b4bec0e578f9d4cd15f44931
wildRaccoon <chukvm@gmail.com>
2020-02-04 12:16:56 +0200

    add comment for ref by func

--- src/ev.lib/domain/app/services/reference/IReferenceService.cs
+++ src/ev.lib/domain/app/services/reference/IReferenceService.cs
@@ -7,6 +7,13 @@ namespace ev.lib.domain.app.services.reference
         where T : IIdEntity
     {
         IEntityRef<T> GetRef(string id);
+        /// <summary>
+        /// find entity by filter on property
+        /// </summary>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="prop">filter by property</param>
+        /// <param name="val">value for comparison</param>
+        /// <returns></returns>
         IEntityRef<T> GetRef<TOut>(Func<T, TOut> prop, TOut val);
         /// <summary>
         /// get reference for new instance
